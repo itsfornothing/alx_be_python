8b60079af88bb8fbffaded7aaad27fe568a9eb0f
Halid Mohamed <150917290+itsfornothing@users.noreply.github.com>
2025-01-10 21:35:21 +0300

    Update temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -3,13 +3,11 @@ CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
 
 def convert_to_celsius(fahrenheit):
-    global FAHRENHEIT_TO_CELSIUS_FACTOR
     celsius = FAHRENHEIT_TO_CELSIUS_FACTOR * (fahrenheit - 32)
     print(f"{fahrenheit} is {celsius}°C")
 
 
 def convert_to_fahrenheit(celsius):
-    global CELSIUS_TO_FAHRENHEIT_FACTOR
     fahrenheit = (CELSIUS_TO_FAHRENHEIT_FACTOR * celsius) + 32
     print(f"{celsius} is {fahrenheit}°F")
 
